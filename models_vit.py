40e8d6374fb7fbd15598ebed9d4c7b8179a2ef45
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-06-27 13:53:31 +0100

    Update models_vit.py

--- models_vit.py
+++ models_vit.py
@@ -15,12 +15,55 @@ from util.pos_embed import get_2d_sincos_pos_embed
 
 from torch.jit import Final
 from functools import partial
-from timm.models.layers import PatchEmbed, Mlp, DropPath, use_fused_attn
+from timm.models.layers import DropPath, use_fused_attn
 import torch.nn.functional as F
 
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
+
+# Patch Embedding without padding
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        return x
+
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
 
 # Attention with return attention
 class Attention(nn.Module):
